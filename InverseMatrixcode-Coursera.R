fb023bf8c4fe862dbad1cd0d54f902fd42545e73
meghansa01 <49802507+meghansa01@users.noreply.github.com>
2019-12-28 13:37:50 -0500

    Attempt #2
    Added Explanations

--- InverseMatrixcode-Coursera.R
+++ InverseMatrixcode-Coursera.R
@@ -1,17 +1,20 @@
+#Attempting to make a cahced martix
 makeCacheMatrix <- function(x = matrix()) {
-  matrix <- matrix()
+  inv <- NULL
   set <- function(y) {
     x <<- y
     m <<- matrix()
   }
   get <- function() x
-  setinverse <- function(matrix) m <<- matrix
-  getinverse <- function(solve) m
+  #giving ability to set and retrieve inverse of matrix in funtion
+  setinverse <- function(inv) inv <<- inverse
+  getinverse <- function() inv
   list(set = set, get = get,
-       setsolve = setsolve,
-       getsolve = getsolve)
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
+#Attempting the function for computing the inverse of the matrix via the "solve" function
 cachesolve <- function(x, ...) {
   m <- x$getsolve()
   if(!is.null(m)) {
